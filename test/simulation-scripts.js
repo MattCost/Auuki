42b6c81a739246ef32d5ffe8bd1c6c1bfdc6eb77
Matt Costuros <matt.costuros@gmail.com>
2025-12-25 12:13:12 -0500

    Updated simulation for testing

--- test/simulation-scripts.js
+++ test/simulation-scripts.js
@@ -1,5 +1,13 @@
 import { xf, equals, rand } from '../src/functions.js';
 
+const PowerErrorMode = Object.freeze({
+    None: 0,
+    Offset: 1,
+    Scale: 2,
+    OffsetAndScale: 3,
+    Ranged: 4,
+});
+
 class TrainerMock {
     constructor() {
         this.defaults = {
@@ -8,11 +16,12 @@ class TrainerMock {
             ftp: 256,
         };
         this.powerTarget = this.defaults.powerTarget;
+        this.powerTargetTrainer = this.defaults.powerTarget;
         this.slope = this.defaults.slope;
         this.ftp = this.defaults.ftp;
 
         this.cadence = 80;
-        this.power = 220;
+        this.power = 0;
         this.speed = 20;
         this.heartRate = 140;
 
@@ -21,12 +30,22 @@ class TrainerMock {
             'one': 0.54, 'two': 0.75, 'three': 0.87,
             'four': 0.94, 'five': 1.05, 'six': 1.20,
         };
+
+        this.powerErrorMode = PowerErrorMode.Scale;
+        this.powerErrorOffset = 0;
+        this.powerErrorScale = 0.85;
+        this.powerErrorRange = [
+            { 'low': 0, 'high': 100, 'offset': 20, 'scale': 1.0 },
+            { 'low': 100, 'high': 200, 'offset': 35, 'scale': 1.0 },
+            { 'low': 200, 'high': 999, 'offset': 40, 'scale': 1.0 },
+        ];
         // this.init();
     }
     init() {
         const self = this;
 
         xf.sub('db:powerTarget', self.onPowerTarget.bind(self));
+        xf.sub('db:powerTargetTrainer', self.onPowerTargetTrainer.bind(self));
         xf.sub('db:slopeTarget', self.onSlopeTarget.bind(self));
         xf.sub('db:ftp', self.onFTP.bind(self));
 
@@ -79,8 +98,12 @@ class TrainerMock {
     }
     onPowerTarget(powerTarget) {
         this.powerTarget = powerTarget;
-        this.power = powerTarget > 0 ? powerTarget : this.defaults.powerTarget;
+        // this.power = powerTarget > 0 ? powerTarget : this.defaults.powerTarget;
         this.heartRate = this.powerToHeartRate(powerTarget, this.ftp, this.zones);
+    }
+    onPowerTargetTrainer(powerTarget) {
+        this.powerTargetTrainer = powerTarget;
+        this.power = this.addErrorToPower(powerTarget);
     }
     onSlopeTarget(slope) {
         this.slope = slope;
@@ -89,7 +112,7 @@ class TrainerMock {
         this.ftp = ftp;
     }
     powerNext(prev) {
-        return this.powerTarget;
+        return this.power;
     }
     cadenceNext(prev) {
         return prev + rand(-1, 1);
@@ -99,25 +122,25 @@ class TrainerMock {
     }
     powerToHeartRate(power, ftp, zones) {
         let base = 90;
-        if(equals(this.powerToZone(power, ftp, zones).name, 'one')) {
+        if (equals(this.powerToZone(power, ftp, zones).name, 'one')) {
             base = 100;
         }
-        if(equals(this.powerToZone(power, ftp, zones).name, 'two')) {
+        if (equals(this.powerToZone(power, ftp, zones).name, 'two')) {
             base = 130;
         }
-        if(equals(this.powerToZone(power, ftp, zones).name, 'three')) {
+        if (equals(this.powerToZone(power, ftp, zones).name, 'three')) {
             base = 150;
         }
-        if(equals(this.powerToZone(power, ftp, zones).name, 'four')) {
+        if (equals(this.powerToZone(power, ftp, zones).name, 'four')) {
             base = 160;
         }
-        if(equals(this.powerToZone(power, ftp, zones).name, 'five')) {
+        if (equals(this.powerToZone(power, ftp, zones).name, 'five')) {
             base = 170;
         }
-        if(equals(this.powerToZone(power, ftp, zones).name, 'six')) {
+        if (equals(this.powerToZone(power, ftp, zones).name, 'six')) {
             base = 180;
         }
-        if(equals(this.powerToZone(power, ftp, zones).name, 'seven')) {
+        if (equals(this.powerToZone(power, ftp, zones).name, 'seven')) {
             base = 190;
         }
         return base;
@@ -127,19 +150,19 @@ class TrainerMock {
 
         let index = 0;
         let name = zones[index];
-        if(value < (ftp * self.percentages.one)) {
+        if (value < (ftp * self.percentages.one)) {
             index = 0;
             name = zones[index];
-        } else if(value < (ftp * self.percentages.two)) {
+        } else if (value < (ftp * self.percentages.two)) {
             index = 1;
             name = zones[index];
-        } else if(value < (ftp * self.percentages.three)) {
+        } else if (value < (ftp * self.percentages.three)) {
             index = 2;
             name = zones[index];
-        } else if(value < (ftp * self.percentages.four)) {
+        } else if (value < (ftp * self.percentages.four)) {
             index = 3;
             name = zones[index];
-        } else if(value < (ftp * self.percentages.five)) {
+        } else if (value < (ftp * self.percentages.five)) {
             index = 4;
             name = zones[index];
         } else if (value < (ftp * self.percentages.six)) {
@@ -149,7 +172,36 @@ class TrainerMock {
             index = 6;
             name = zones[index];
         }
-        return {name, index};
+        return { name, index };
+    }
+
+    addErrorToPower(powerTarget) {
+        switch (this.powerErrorMode) {
+            case PowerErrorMode.None:
+                return powerTarget;
+
+            case PowerErrorMode.Offset:
+                return powerTarget + this.powerErrorOffset;
+
+            case PowerErrorMode.Scale:
+                return powerTarget * this.powerErrorScale;
+
+            case PowerErrorMode.OffsetAndScale:
+                return (powerTarget * this.powerErrorScale) + this.powerErrorOffset;
+
+            case PowerErrorMode.Ranged:
+                for (let i = 0; i < this.powerErrorRange.length; i++) {
+                    if (powerTarget >= this.powerErrorRange[i].low && powerTarget < this.powerErrorRange[i].high)
+                        return (powerTarget * this.powerErrorRange[i].scale) + this.powerErrorRange[i].offset;
+                }
+                return (powerTarget * this.powerErrorRange[ this.powerErrorRange.length-1].scale) + this.powerErrorRange[this.powerErrorRange.length-1].offset;
+
+            default:
+                return powerTarget;
+
+        }
+
+
     }
 }
 
